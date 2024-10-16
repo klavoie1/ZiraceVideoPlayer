38694622a16e60b0dba2512117e18ac859d573da
Kenneth <129323640+klavoie1@users.noreply.github.com>
2024-10-16 19:34:36 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ZiraceVideoPlayer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
