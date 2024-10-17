fb68bcc74328715c757e2a5251c0d5ddf36b9587
Kenneth <129323640+klavoie1@users.noreply.github.com>
2024-10-16 20:22:37 -0400

    Added the initial workings of a playlist

---
+++ Services/PlaylistManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ZiraceVideoPlayer.Models;
+
+namespace ZiraceVideoPlayer.Services
+{
+    public class PlaylistManager
+    {
+        private const string PlaylistFilePath = "playlists.json";
+
+        public async Task SavePlaylistsAsync(List<Playlist> playlists)
+        {
+            var json = JsonSerializer.Serialize(playlists);
+            await File.WriteAllTextAsync(PlaylistFilePath, json);
+        }
+
+        public async Task<List<Playlist>> LoadPlaylistsAsync()
+        {
+            if (!File.Exists(PlaylistFilePath))
+                return new List<Playlist>();
+
+            var json = await File.ReadAllTextAsync(PlaylistFilePath);
+            return JsonSerializer.Deserialize<List<Playlist>>(json);
+        }
+        
+    }
+}
