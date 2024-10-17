22e8d19dc86f575dfcb8f0b99bf48dd0599d51a8
Kenneth <129323640+klavoie1@users.noreply.github.com>
2024-10-16 20:22:11 -0400

    Created classes for future implementing

---
+++ Models/Playlist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZiraceVideoPlayer.Models
+{
+    public class Playlist
+    {
+        public string Name { get; set; }
+        public List<Video> Videos { get; set; } = new List<Video>();
+    }
+}
---
+++ Models/Video.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZiraceVideoPlayer.Models
+{
+        public class Video
+        {
+            public required string Title { get; set; }
+            public required string FilePath { get; set; }
+            public required TimeSpan Duration { get; set; }
+            // Thumbnail is not Required
+            public string? Thumbnail { get; set; }
+        }
+}
