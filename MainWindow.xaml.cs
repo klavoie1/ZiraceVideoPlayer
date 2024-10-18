069b8afd577fc7fe74cfa7193fd72f37911e3825
klavoie1 <kenlavoie19@gmail.com>
2024-10-17 20:53:45 -0400

    Added minimum functionality for app

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Printing;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -24,7 +25,7 @@ namespace ZiraceVideoPlayer
 
         //Dispatcher Elements
         private DispatcherTimer durationTimer; //Timing for the Duration Slider
-
+        private DispatcherTimer DurationLabelTimer; // Timing for duration labels
 
 
         public MainWindow()
@@ -35,6 +36,14 @@ namespace ZiraceVideoPlayer
             durationTimer = new DispatcherTimer();
             durationTimer.Interval = TimeSpan.FromMilliseconds(500);
             durationTimer.Tick += Timer_Tick;
+
+            DurationLabelTimer = new DispatcherTimer();
+            DurationLabelTimer.Interval = TimeSpan.FromSeconds(2);
+            DurationLabelTimer.Tick += Timer_Tick;
+
+            volumeSlider.Value = 0.3; // Set default volume to 30%
+            mediaElement.Volume = volumeSlider.Value; // Sync initial volume
+            volumeSlider.Value = mediaElement.Volume;
         }
 
         
@@ -50,6 +59,7 @@ namespace ZiraceVideoPlayer
             {
                 mediaElement.Source = new Uri(openFileDialog.FileName);
                 mediaElement.Play();
+                durationTimer.Start();
             }
         }
 
@@ -75,10 +85,10 @@ namespace ZiraceVideoPlayer
 
 
         // End of File menu bar settings------------------------------------------------------------>
-        
+
 
         // OverLay Control Panel Settings---------------------------------------------------------->
-    
+
         // End of Overlay Control Panel Settings--------------------------------------------------->
 
 
@@ -88,8 +98,27 @@ namespace ZiraceVideoPlayer
         private void Timer_Tick(object sender, EventArgs e)
         {
             if (mediaElement.NaturalDuration.HasTimeSpan && !isSeeking)
-        }
+            {
+                isTimerUpdate = true;
+                DurationSlider.Maximum = mediaElement.NaturalDuration.TimeSpan.TotalSeconds;
+                DurationSlider.Value = mediaElement.Position.TotalSeconds;
+                isTimerUpdate = false;
+            }
+            if (mediaElement.NaturalDuration.HasTimeSpan)
+            {
+                // Update the elapsed time
+                TimeSpan currentTime = mediaElement.Position;
+                ElapsedTime.Text = FormatTime(currentTime);
 
+                // Update the remaining time
+                TimeSpan remaining = mediaElement.NaturalDuration.TimeSpan - currentTime;
+                RemainingTime.Text = $"-{FormatTime(remaining)}";
+
+                // Update the slider value based on the current position
+                DurationSlider.Value = currentTime.TotalSeconds;
+                DurationSlider.Maximum = mediaElement.NaturalDuration.TimeSpan.TotalSeconds;
+            }
+        }
 
 
         private void btnPlay_Click(object sender, RoutedEventArgs e)
@@ -124,16 +153,17 @@ namespace ZiraceVideoPlayer
             mediaElement.Volume = volumeSlider.Value;
         }
 
-        private void DurationSlider_PreviewMouseDown(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void DurationSlider_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             isSeeking = true; // Stops the timer when a user interacts with the slider
-            // Need to set the labels for the slider
+            DurationLabelTimer.Stop();
         }
 
-        private void DurationSlider_PreviewMouseUp(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void DurationSlider_PreviewMouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             isSeeking = false; // Resumes the timer updates
             mediaElement.Position = TimeSpan.FromSeconds(DurationSlider.Value);
+            DurationLabelTimer.Start();
         }
 
         private void DurationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -144,12 +174,78 @@ namespace ZiraceVideoPlayer
             }
         }
 
+        private void ToggleFullscreen()
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+                this.WindowStyle = WindowStyle.None; // Optional: Hide window borders
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+                this.WindowStyle = WindowStyle.SingleBorderWindow; // Restore borders
+            }
+        }
+
+        private void IncreaseVolume()
+        {
+            if (mediaElement.Volume < 1)
+            {
+                mediaElement.Volume = Math.Min(mediaElement.Volume + 0.1, 1.0); // Max Volume is 1
+                volumeSlider.Value = mediaElement.Volume;
+                Console.WriteLine($"Volume: {mediaElement.Volume * 100}%");
+            }
+        }
+
+        private void DecreaseVolume()
+        {
+            if (mediaElement.Volume > 0.0)
+            {
+                mediaElement.Volume = Math.Max(mediaElement.Volume - 0.1, 0.0); //Min Volume is 0
+                volumeSlider.Value = mediaElement.Volume;
+                Console.WriteLine($"Volume: {mediaElement.Volume * 100}%");
+            }
+        }
+
+        // End of Control Panel Settings------------------------------------------------------->
+
+
+        // Keyboard Shortcut Settings---------------------------------------------->
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.F:
+                    ToggleFullscreen();
+                    e.Handled = true;
+                    break;
+
+                case Key.Up:
+                    IncreaseVolume();
+                    e.Handled = true;
+                    break;
+
+                case Key.Down:
+                    DecreaseVolume();
+                    e.Handled = true;
+                    break;
+
+
+                
+            }
+        }
 
 
 
 
+        // Helper Methods------------------------------------------------------>
 
+        // Helper method to format the time (hh:mm:ss)
+        private string FormatTime(TimeSpan time)
+        {
+            return time.ToString(time.Hours > 0 ? @"hh\:mm\:ss" : @"mm\:ss");
+        }
 
-
-    }
+        }
 }
