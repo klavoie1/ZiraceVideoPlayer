af4aab3d133bc703593950f4ab57eb982b53f163
Kenneth <129323640+klavoie1@users.noreply.github.com>
2024-10-19 21:10:24 -0400

    Converter for XAML processes

---
+++ Services/SliderValueToWidthConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ZiraceVideoPlayer.Services
+{
+    public class ValueToWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double sliderValue = (double)value;
+            double maxWidth = 800; // Assuming the width of the Slider is 800.
+            return sliderValue * maxWidth / 100; // Convert value to width proportionally.
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
