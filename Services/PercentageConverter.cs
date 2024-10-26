bdb43c0aa6191c7e14ce74cbbba78b2c6528405f
Kenneth <129323640+klavoie1@users.noreply.github.com>
2024-10-26 19:22:18 -0400

    Converter for UI elements

---
+++ Services/PercentageConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+using System.Windows.Data;
+
+namespace ZiraceVideoPlayer.Services
+{
+    public class PercentageConverter : IValueConverter
+    {
+        public double Percentage { get; set; } = 1.0;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double originalWidth)
+            {
+                return originalWidth * Percentage;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
